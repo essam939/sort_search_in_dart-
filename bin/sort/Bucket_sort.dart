6c2f56e3939ab1a9888a3c6568b442b1a6eeddf8
Mohamed Essam <mohamed.essam9393@gmail.com>
2023-01-25 10:43:53 +0200

    edit

--- bin/sort/Bucket_sort.dart
+++ bin/sort/Bucket_sort.dart
@@ -1,14 +1,14 @@
-void main() {
-  // A distribution sort algorithm that separates the elements of an array into a number
-  // of buckets, then sorts each bucket individually, either using a different sorting algorithm 
-  //or by recursively applying the bucket sorting algorithm.
+ main() {
+  // A distribution sort algorithm that separates the elements of an array into a number of buckets,
+  //then sorts each bucket individually,
+  // either using a different sorting algorithm or by recursively applying the bucket sorting algorithm.
   var data = [0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434];
   print(bucketSort(data));
 }
 
 List<double> bucketSort(List<double> arr) {
   var n = arr.length;
-  var buckets = List.generate(n, (i) => []);
+  var buckets = List.generate(n, (i) => <double>[]);
 
   for (var i = 0; i < n; i++) {
     var index = (n * arr[i]).floor();
